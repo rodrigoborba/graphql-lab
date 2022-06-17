13cf8244f6ece09dfe6a8df95672e144f6cb4e9b
Rodrigo Borba <rodrigo.borba@grupoboticario.com.br>
2022-06-17 20:57:48 -0300

    feat: add basic interaction between frontend, apollo client and graphQL backend

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+
+import {useQuery, gql} from '@apollo/client';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading, error, data} = useQuery(TRACKS);
+
+  if (loading) return 'Loading...';
+
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>
 };
 
 export default Tracks;
